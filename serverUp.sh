69298fe502b8b3a74684a21c6be065991c8ea6d9
サーバー エラー <35196376+2340958@users.noreply.github.com>
2021-11-19 14:47:11 +0100

    Update serverUp.sh

--- serverUp.sh
+++ serverUp.sh
@@ -17,13 +17,13 @@ do
             echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > redirection ${curl}";
     elif [[ $curl == "000" ]]
         then
-            echo "WARNING: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > DNS error - check domain in server list" >> /var/log/serverUp.log 2>&1;
+            echo "WARNING: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > dns error - check domain in server list" >> /var/log/serverUp.log 2>&1;
     elif [[ $curl == 4* ]]
         then
             echo "WARNING: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > client errors ${curl}" >> /var/log/serverUp.log 2>&1;
     elif [[ $curl == 5* ]]
         then
-            echo "ERROR: ${server} down! $(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" >> /var/log/serverUp.log 2>&1;
+            echo "ERROR: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} down > statuscode ${curl}" >> /var/log/serverUp.log 2>&1;
             curl \
             -X POST \
             -s \
@@ -35,7 +35,7 @@ do
             "https://api.telegram.org/bot${botToken}/sendMessage" \
             > /dev/null
     else
-        echo "ERROR: ${server} down! $(date +"%a, %d. %B %Y, %H:%M:%S") > other error ${curl} >> NO MESSAGE SENT" >> /var/log/serverUp.log 2>&1;
+        echo "ERROR: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} down > other error ${curl} >> NO MESSAGE SENT" >> /var/log/serverUp.log 2>&1;
     fi
 done
 
