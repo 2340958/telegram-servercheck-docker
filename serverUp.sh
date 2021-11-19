c93595ee7822c5276695f9066f11853370f7f993
サーバー エラー <35196376+2340958@users.noreply.github.com>
2021-11-19 12:34:04 +0100

    Update serverUp.sh
    nc is nice and all. but more complex web apps and load balancer answer as well and you wont get 300 and 500 http errors with the old variant

--- serverUp.sh
+++ serverUp.sh
@@ -8,22 +8,36 @@ chatId=""
 
 for server in $(echo $serverList | sed "s/,/ /g")
 do
-    if ! nc -w 2 -z $server $port > /dev/null 2>&1
+    curl=$(curl -sL -w "%{http_code}\\n" "$server" -o /dev/null)
+    echo "${server}:${curl}"
+    if [[ $curl == "200" ]]
+        then 
+            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > OK ${curl}" 
+    elif [[ $curl == "301" ]]
         then
-            echo "ERROR: ${server}:${port} down! $(date +"%a, %d. %B %Y, %H:%M:%S")" >> /var/log/serverUp.log 2>&1;
-            curl \
-            -X POST \
-            -s \
-            --data "chat_id=${chatId}" \
-            --data "disable_web_page_preview=true" \
-            --data "text=Server ${server}:${port} down!%0A$(date +"%a, %d. %B %Y, %H:%M:%S")" \
-            --connect-timeout 30 \
-            --max-time 45 \
-            "https://api.telegram.org/bot${botToken}/sendMessage" \
-            > /dev/null
+            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > redirect ${curl}"
+    elif [[ $curl == "305" ]]
+        then
+            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > redirect ${curl}"
+    elif [[ $curl == "000" ]]
+        then
+            echo "WARNING: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > DNS error - check domain in server list" >> /var/log/serverUp.log 2>&1;
+    elif [[ $curl == "404" ]]
+        then
+            echo "ERROR: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > page not found ${curl}" >> /var/log/serverUp.log 2>&1;
     else
-        echo "INFO: ${server}:${port} up! $(date +"%a, %d. %B %Y, %H:%M:%S")" >> /var/log/serverUp.log 2>&1
+        echo "ERROR: ${server} down! $(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" >> /var/log/serverUp.log 2>&1;
+        curl \
+        -X POST \
+        -s \
+        --data "chat_id=${chatId}" \
+        --data "disable_web_page_preview=true" \
+        --data "text=Server ${server} down!%0A$(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" \
+        --connect-timeout 30 \
+        --max-time 45 \
+        "https://api.telegram.org/bot${botToken}/sendMessage" \
+        > /dev/null
     fi
 done
 
-echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): executed script" >> /var/log/serverUp.log 2>&1
+echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): executed script" >> /var/log/serverUp.log 2>&1;
