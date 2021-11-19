60acf104e1d1d0cb0bd8af6708e528e36c1035a5
サーバー エラー <35196376+2340958@users.noreply.github.com>
2021-11-19 14:44:11 +0100

    Update serverUp.sh
    fix for different statuscodes

--- serverUp.sh
+++ serverUp.sh
@@ -9,33 +9,33 @@ chatId=""
 for server in $(echo $serverList | sed "s/,/ /g")
 do
     curl=$(curl -s -w "%{http_code}\\n" "$server" -o /dev/null)
-    if [[ $curl == "200" ]]
+    if [[ $curl == 2* ]]
         then 
-            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > OK ${curl}" 
-    elif [[ $curl == "301" ]]
+            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > success ${curl}" ;
+    elif [[ $curl == 3* ]]
         then
-            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > redirect ${curl}"
-    elif [[ $curl == "305" ]]
-        then
-            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > redirect ${curl}"
+            echo "INFO: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > redirection ${curl}";
     elif [[ $curl == "000" ]]
         then
             echo "WARNING: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > DNS error - check domain in server list" >> /var/log/serverUp.log 2>&1;
-    elif [[ $curl == "404" ]]
+    elif [[ $curl == 4* ]]
         then
-            echo "ERROR: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > page not found ${curl}" >> /var/log/serverUp.log 2>&1;
+            echo "WARNING: $(date +"%a, %d. %B %Y, %H:%M:%S"): ${server} > client errors ${curl}" >> /var/log/serverUp.log 2>&1;
+    elif [[ $curl == 5* ]]
+        then
+            echo "ERROR: ${server} down! $(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" >> /var/log/serverUp.log 2>&1;
+            curl \
+            -X POST \
+            -s \
+            --data "chat_id=${chatId}" \
+            --data "disable_web_page_preview=true" \
+            --data "text=Server ${server} down!%0A$(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" \
+            --connect-timeout 30 \
+            --max-time 45 \
+            "https://api.telegram.org/bot${botToken}/sendMessage" \
+            > /dev/null
     else
-        echo "ERROR: ${server} down! $(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" >> /var/log/serverUp.log 2>&1;
-        curl \
-        -X POST \
-        -s \
-        --data "chat_id=${chatId}" \
-        --data "disable_web_page_preview=true" \
-        --data "text=Server ${server} down!%0A$(date +"%a, %d. %B %Y, %H:%M:%S") > statuscode ${curl}" \
-        --connect-timeout 30 \
-        --max-time 45 \
-        "https://api.telegram.org/bot${botToken}/sendMessage" \
-        > /dev/null
+        echo "ERROR: ${server} down! $(date +"%a, %d. %B %Y, %H:%M:%S") > other error ${curl} >> NO MESSAGE SENT" >> /var/log/serverUp.log 2>&1;
     fi
 done
 
